2466147e9b33971912f910059e5106490042a4b6
erikson <erikson.velro@gmail.com>
2023-01-24 17:07:51 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FoodComponent } from './components/food/food.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ChangeComponent } from './components/change/change.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FoodComponent,
+    CheckoutComponent,
+    ChangeComponent,
+    ProfileComponent,
+    HistoryComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
