89f101739f9f7b681eb491b12aa39949243ef6d1
erikson <erikson.velro@gmail.com>
2023-01-25 23:26:46 -0500

    food updates

--- src/app/components/food/food.component.ts
+++ src/app/components/food/food.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Food } from 'src/app/models/food.model';
 
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
-  styleUrls: ['./food.component.css']
+  styleUrls: ['./food.component.css'],
 })
 export class FoodComponent {
-
+  currentFood!: Food;
+  foodId: string | null = null;
 }
