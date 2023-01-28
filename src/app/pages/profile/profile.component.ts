b93ea96a4aeb54e8fac228c4ed24fa16391ddb8c
Maribel <maribel.torres.guevara@gmail.com>
2023-01-28 18:42:21 -0500

    completado el perfil

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Profile } from 'src/app/models/profile.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
-  name : string = 'Margarita Flores';
+export class ProfileComponent implements OnInit {
+  profile!: Profile;
+
+  constructor(
+    private profileService: ProfileService,
+    public authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.obtenerPerfil();
+  }
+
+  obtenerPerfil() {
+    this.profileService.getProfile().subscribe((profile: any) => {
+      this.profile = profile;
+    });
+  }
 }
